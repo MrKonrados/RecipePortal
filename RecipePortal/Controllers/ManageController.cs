6caa3db816c7ffee42f84e4343c7ec07c4c2d2f2
MrKonrados <konrad.slizewski@gmail.com>
2018-11-25 18:04:08 +0100

    Move classes to separete files

--- RecipePortal/Controllers/ManageController.cs
+++ RecipePortal/Controllers/ManageController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using RecipePortal.Models;
+using RecipePortal.ViewModels;
 
 namespace RecipePortal.Controllers
 {
---
+++ RecipePortal/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipePortal.ViewModels
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
